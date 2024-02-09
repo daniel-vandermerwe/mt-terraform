25d30a508f9e3b8c76e1eb8e41ab325c85960aed
Daniel van der Merwe <daniel.van.der.merwe@evolutiops.com>
2024-02-09 17:08:00 -0500

    Add harness provider config

--- terraform/modules/connectors/variables.tf
+++ terraform/modules/connectors/variables.tf
@@ -13,3 +13,19 @@ variable "git_validation_repo" {
   description = "Repo to use for git connector validation"
   type        = string
 }
+
+variable "harness_endpoint" {
+  description = "The Harness Endpoint"
+  type        = string
+  default     = "https://app.harness.io/gateway"
+}
+
+variable "harness_account_id" {
+  description = "The Harness Account ID"
+  type        = string
+}
+
+variable "harness_api_key" {
+  description = "The Harness API Key"
+  type        = string
+}
